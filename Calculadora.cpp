df9d5d9198e081df9b86fc926f56907d99f3574f
SalcedoSamuel <salcedolucerosamuel2@gmail.com>
2024-03-02 18:26:46 -0600

    Primer commit

---
+++ Calculadora.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+
+void Suma();
+void Resta();
+void Multiplicacion();
+void Division();
+
+
+int main()  
+{
+	int opcion;
+	cout<<"Elige una opcion \n";
+	cout<<"1.-Suma \n";
+	cout<<"2.-Resta \n";
+	cout<<"3.-Multiplicacion \n";
+	cout<<"4.-Division \n";
+	cin>>opcion;
+	switch (opcion) 
+	{
+    case 1:
+        Suma();
+        break;
+    case 2:
+        Resta();
+        break;
+	case 3:
+		Multiplicacion();
+		break;
+	case 4:
+		Division();
+		break;   
+    default:
+        cout<<"No se encuentra la opcion introducida";
+	}
+    return 0;
+}
+void Suma(){
+	int numero1, numero2, resultado;
+    cout<<"Introduce el primer numero: ";
+    cin>>numero1;
+    cout<<"Introduce el segundo numero: ";
+    cin>>numero2;
+    resultado=numero1+numero2;
+    cout<<"El resultado es: "<<resultado;
+}
+void Resta(){
+	int numero1, numero2, resultado;
+    cout<<"Introduce el primer numero: ";
+    cin>>numero1;
+    cout<<"Introduce el segundo numero: ";
+    cin>>numero2;
+    resultado=numero1-numero2;
+    cout<<"El resultado es: "<<resultado;
+}
+
+void Multiplicacion(){  
+    int numero1, numero2, resultado;
+    cout<<"Introduce el primer numero: ";
+    cin>>numero1;
+    cout<<"Introduce el segundo numero: ";
+    cin>>numero2;
+    resultado=numero1*numero2;
+    cout<<"El resultado es: "<<resultado;
+}
+void Division(){
+	int numero1, numero2, resultado;
+    cout<<"Introduce el primer numero: ";
+    cin>>numero1;
+    cout<<"Introduce el segundo numero: ";
+    cin>>numero2;
+    resultado=numero1/numero2;
+    cout<<"El resultado es: "<<resultado;
+}
+
